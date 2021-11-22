3544c1d1218798630b82f219dd2d24ba5b462b30
itsubaki <1759459+itsubaki@users.noreply.github.com>
2021-11-22 18:48:11 +0900

    Add some files

--- cmd/action.go
+++ cmd/action.go
@@ -1,14 +1,12 @@
 package cmd
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"time"
 
-	"github.com/google/go-github/v40/github"
+	"github.com/itsubaki/prstats/pkg/prstats"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/oauth2"
 )
 
 type PRStats struct {
@@ -62,146 +60,17 @@ func (s PRStats) JSON() string {
 }
 
 func Action(c *cli.Context) error {
-	owner := c.String("owner")
-	repo := c.String("repo")
-
-	ctx := context.Background()
-	client := github.NewClient(nil)
-
-	pat := c.String("pat")
-	if pat != "" {
-		client = github.NewClient(oauth2.NewClient(ctx, oauth2.StaticTokenSource(
-			&oauth2.Token{AccessToken: pat},
-		)))
+	in := prstats.GetStatsInput{
+		Owner:   c.String("owner"),
+		Repo:    c.String("repo"),
+		PAT:     c.String("pat"),
+		State:   c.String("state"),
+		PerPage: c.Int("perpage"),
 	}
 
-	var stats PRStats
-	{
-
-		opt := github.PullRequestListOptions{
-			State: c.String("state"),
-			ListOptions: github.ListOptions{
-				PerPage: c.Int("perpage"),
-			},
-		}
-
-		list := make([]*github.PullRequest, 0)
-		for {
-			pr, resp, err := client.PullRequests.List(ctx, owner, repo, &opt)
-			if err != nil {
-				return fmt.Errorf("list PR: %v", err)
-			}
-
-			list = append(list, pr...)
-			if resp.NextPage == 0 {
-				break
-			}
-
-			opt.Page = resp.NextPage
-		}
-
-		stats.Owner = owner
-		stats.Repo = repo
-		stats.Range.Beg = list[0].CreatedAt
-		stats.Range.End = list[len(list)-1].CreatedAt
-
-		stats.PerDay.Count = len(list)
-		stats.PerDay.Days = int(list[0].CreatedAt.Sub(*list[len(list)-1].CreatedAt).Hours() / 24)
-		stats.PerDay.CountPerDay = float64(stats.PerDay.Count) / float64(stats.PerDay.Days)
-
-		var count int
-		var sum float64
-		for _, r := range list {
-			if r.MergedAt == nil {
-				continue
-			}
-
-			count++
-			sum += r.MergedAt.Sub(*r.CreatedAt).Hours()
-		}
-
-		stats.Merged.Count = count
-		stats.Merged.Days = int(list[0].MergedAt.Sub(*list[len(list)-1].MergedAt).Hours() / 24)
-		stats.Merged.CountPerDay = float64(stats.Merged.Count) / float64(stats.Merged.Days)
-
-		stats.Merged.HoursPerCount = int(sum / float64(count))
-		stats.Merged.TotalHours = int(sum)
-	}
-
-	{
-		opt := github.ListWorkflowRunsOptions{
-			ListOptions: github.ListOptions{
-				PerPage: c.Int("perpage"),
-			},
-		}
-
-		wmap := make(map[int64][]*github.WorkflowRun)
-		for {
-			runs, resp, err := client.Actions.ListRepositoryWorkflowRuns(ctx, owner, repo, &opt)
-			if err != nil {
-				return fmt.Errorf("list Workflow Runs: %v", err)
-			}
-
-			for _, r := range runs.WorkflowRuns {
-				if r.Conclusion == nil {
-					continue
-				}
-
-				runs, ok := wmap[*r.WorkflowID]
-				if !ok {
-					wmap[*r.WorkflowID] = make([]*github.WorkflowRun, 0)
-				}
-
-				wmap[*r.WorkflowID] = append(runs, r)
-			}
-
-			if resp.NextPage == 0 {
-				break
-			}
-
-			opt.Page = resp.NextPage
-		}
-
-		for k, v := range wmap {
-			if len(v) < 1 {
-				continue
-			}
-
-			var success, failure, skipped, cancelled int
-			for _, r := range v {
-				if *r.Conclusion == "success" {
-					success++
-					continue
-				}
-
-				if *r.Conclusion == "failure" {
-					failure++
-					continue
-				}
-
-				if *r.Conclusion == "skipped" {
-					skipped++
-					continue
-				}
-
-				if *r.Conclusion == "cancelled" {
-					cancelled++
-					continue
-				}
-			}
-
-			stats.Workflow = append(stats.Workflow, Workflow{
-				ID:          k,
-				Name:        *v[0].Name,
-				FailureRate: float64(failure) / float64(len(v)),
-				Count:       len(v),
-				Success:     success,
-				Failure:     failure,
-				Skipped:     skipped,
-				Cancelled:   cancelled,
-			})
-		}
-
+	stats, err := prstats.GetStats(&in)
+	if err != nil {
+		return fmt.Errorf("get stats: %v", err)
 	}
 
 	fmt.Println(stats)
---
+++ pkg/prstats/stats.go
@@ -0,0 +1,209 @@
+package prstats
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/google/go-github/v40/github"
+	"golang.org/x/oauth2"
+)
+
+type Workflow struct {
+	ID          int64   `json:"id"`
+	Name        string  `json:"name"`
+	FailureRate float64 `json:"failure_rate"`
+	Count       int     `json:"count"`
+	Success     int     `json:"success"`
+	Failure     int     `json:"failure"`
+	Skipped     int     `json:"skipped"`
+	Cancelled   int     `json:"cancelled"`
+}
+
+type PRStats struct {
+	Owner string `json:"owner"`
+	Repo  string `json:"repo"`
+
+	Range struct {
+		Beg *time.Time `json:"beg"`
+		End *time.Time `json:"end"`
+	} `json:"range"`
+
+	PerDay struct {
+		CountPerDay float64 `json:"count_per_day"`
+		Count       int     `json:"count"`
+		Days        int     `json:"days"`
+	} `json:"pr"`
+
+	Merged struct {
+		CountPerDay   float64 `json:"count_per_day"`
+		Count         int     `json:"count"`
+		Days          int     `json:"days"`
+		HoursPerCount int     `json:"hours_per_count"`
+		TotalHours    int     `json:"total_hours"`
+	} `json:"merged"`
+
+	Workflow []Workflow `json:"workflow"`
+}
+
+func (s PRStats) String() string {
+	return s.JSON()
+}
+
+func (s PRStats) JSON() string {
+	b, err := json.Marshal(s)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(b)
+}
+
+type GetStatsInput struct {
+	Owner   string
+	Repo    string
+	PAT     string
+	State   string
+	PerPage int
+}
+
+func GetStats(in *GetStatsInput) (*PRStats, error) {
+	ctx := context.Background()
+	client := github.NewClient(nil)
+
+	if in.PAT != "" {
+		client = github.NewClient(oauth2.NewClient(ctx, oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: in.PAT},
+		)))
+	}
+
+	var stats PRStats
+	{
+		opt := github.PullRequestListOptions{
+			State: in.State,
+			ListOptions: github.ListOptions{
+				PerPage: in.PerPage,
+			},
+		}
+
+		list := make([]*github.PullRequest, 0)
+		for {
+			pr, resp, err := client.PullRequests.List(ctx, in.Owner, in.Repo, &opt)
+			if err != nil {
+				return nil, fmt.Errorf("list PR: %v", err)
+			}
+
+			list = append(list, pr...)
+			if resp.NextPage == 0 {
+				break
+			}
+
+			opt.Page = resp.NextPage
+		}
+
+		stats.Owner = in.Owner
+		stats.Repo = in.Repo
+		stats.Range.Beg = list[0].CreatedAt
+		stats.Range.End = list[len(list)-1].CreatedAt
+
+		stats.PerDay.Count = len(list)
+		stats.PerDay.Days = int(list[0].CreatedAt.Sub(*list[len(list)-1].CreatedAt).Hours() / 24)
+		stats.PerDay.CountPerDay = float64(stats.PerDay.Count) / float64(stats.PerDay.Days)
+
+		var count int
+		var sum float64
+		for _, r := range list {
+			if r.MergedAt == nil {
+				continue
+			}
+
+			count++
+			sum += r.MergedAt.Sub(*r.CreatedAt).Hours()
+		}
+
+		stats.Merged.Count = count
+		stats.Merged.Days = int(list[0].MergedAt.Sub(*list[len(list)-1].MergedAt).Hours() / 24)
+		stats.Merged.CountPerDay = float64(stats.Merged.Count) / float64(stats.Merged.Days)
+
+		stats.Merged.HoursPerCount = int(sum / float64(count))
+		stats.Merged.TotalHours = int(sum)
+	}
+
+	{
+		opt := github.ListWorkflowRunsOptions{
+			ListOptions: github.ListOptions{
+				PerPage: in.PerPage,
+			},
+		}
+
+		wmap := make(map[int64][]*github.WorkflowRun)
+		for {
+			runs, resp, err := client.Actions.ListRepositoryWorkflowRuns(ctx, in.Owner, in.Repo, &opt)
+			if err != nil {
+				return nil, fmt.Errorf("list Workflow Runs: %v", err)
+			}
+
+			for _, r := range runs.WorkflowRuns {
+				if r.Conclusion == nil {
+					continue
+				}
+
+				runs, ok := wmap[*r.WorkflowID]
+				if !ok {
+					wmap[*r.WorkflowID] = make([]*github.WorkflowRun, 0)
+				}
+
+				wmap[*r.WorkflowID] = append(runs, r)
+			}
+
+			if resp.NextPage == 0 {
+				break
+			}
+
+			opt.Page = resp.NextPage
+		}
+
+		for k, v := range wmap {
+			if len(v) < 1 {
+				continue
+			}
+
+			var success, failure, skipped, cancelled int
+			for _, r := range v {
+				if *r.Conclusion == "success" {
+					success++
+					continue
+				}
+
+				if *r.Conclusion == "failure" {
+					failure++
+					continue
+				}
+
+				if *r.Conclusion == "skipped" {
+					skipped++
+					continue
+				}
+
+				if *r.Conclusion == "cancelled" {
+					cancelled++
+					continue
+				}
+			}
+
+			stats.Workflow = append(stats.Workflow, Workflow{
+				ID:          k,
+				Name:        *v[0].Name,
+				FailureRate: float64(failure) / float64(len(v)),
+				Count:       len(v),
+				Success:     success,
+				Failure:     failure,
+				Skipped:     skipped,
+				Cancelled:   cancelled,
+			})
+		}
+	}
+
+	return &stats, nil
+}
