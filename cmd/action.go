493ce3e0eb4f6a47063509517a84bbe2b7cc535c
itsubaki <1759459+itsubaki@users.noreply.github.com>
2021-11-22 09:45:23 +0900

    Update some files

--- cmd/action.go
+++ cmd/action.go
@@ -1,13 +1,28 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/google/go-github/v40/github"
 	"github.com/urfave/cli/v2"
 )
 
 func Action(c *cli.Context) error {
-	fmt.Println("hello world")
+	client := github.NewClient(nil)
+
+	pr, res, err := client.PullRequests.List(context.Background(), "itsubaki", "q", &github.PullRequestListOptions{
+		State: "all",
+	})
+	if err != nil {
+		return fmt.Errorf("list PullRequests: %v", err)
+	}
+
+	fmt.Printf("res: %v\n", res)
+
+	for _, r := range pr {
+		fmt.Printf("ID: %v, title: %v, created: %v, merged: %v, closed: %v\n", *r.ID, *r.Title, r.CreatedAt, r.MergedAt, r.ClosedAt)
+	}
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module github.com/itsubaki/prstats
 
 go 1.17
 
-require github.com/urfave/cli/v2 v2.3.0
+require (
+	github.com/google/go-github/v40 v40.0.0
+	github.com/urfave/cli/v2 v2.3.0
+)
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 )
