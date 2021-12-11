0d95a9c1d2010e76e30ae14d07274503db0e2d6b
itsubaki <1759459+itsubaki@users.noreply.github.com>
2021-12-11 23:13:46 +0900

    Add signal handler

--- appengine/main.go
+++ appengine/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/itsubaki/ghstats/appengine/handler"
 )
@@ -22,8 +26,23 @@ func main() {
 		Handler: handler.New(),
 	}
 
-	log.Println("http server listen and serve")
-	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("listen: %s\n", err)
+	go func() {
+		log.Println("http server listen and serve")
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	ch := make(chan os.Signal, 2)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatalf("http server shutdown: %v\n", err)
 	}
+
+	log.Println("shutdown finished")
 }
