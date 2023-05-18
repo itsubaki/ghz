8b4a073ba926ee11fe9b408130d587e85b35e4f8
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-05-18 12:18:23 +0000

    Add commits/get.go

---
+++ commits/get.go
@@ -0,0 +1,35 @@
+package commits
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/go-github/v50/github"
+	"golang.org/x/oauth2"
+)
+
+type GetInput struct {
+	Owner      string
+	Repository string
+	PAT        string
+	Page       int
+	PerPage    int
+	SHA        string
+}
+
+func Get(ctx context.Context, in GetInput) (*github.RepositoryCommit, error) {
+	client := github.NewClient(nil)
+
+	if in.PAT != "" {
+		client = github.NewClient(oauth2.NewClient(ctx, oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: in.PAT},
+		)))
+	}
+
+	commit, _, err := client.Repositories.GetCommit(ctx, in.Owner, in.Repository, in.SHA, nil)
+	if err != nil {
+		return nil, fmt.Errorf("get commits: %v", err)
+	}
+
+	return commit, nil
+}
